53a23da4f7bf3e0fe1021d9bb25693096d729f47
Zsolt Schutzbach <schutzbach.zsolt@e-vista.hu>
2018-09-06 10:39:52 +0200

    Fixing font awesome import

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,6 +1,7 @@
 import 'bootstrap'
 import 'bootstrap/scss/bootstrap.scss'
 import '@fortawesome/fontawesome-free/scss/fontawesome.scss'
+import '@fortawesome/fontawesome-free/scss/solid.scss'
 
 import ExampleClass from './example.js'
 
